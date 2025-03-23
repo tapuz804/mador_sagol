b14288a90216a3409de937cb2d298bbc7b12793e
Adam Antoshin <adam.antoshin@gmail.com>
2025-03-24 01:00:28 +0200

    Update check_pkt.py

--- projects/encryptor/tashtiot_utils/check_pkt.py
+++ projects/encryptor/tashtiot_utils/check_pkt.py
@@ -1,5 +1,9 @@
 from scapy.all import *
 
+VALID_PKT_FROM_RED = 0
+VALID_PKT_FROM_BLACK = 1
+INVALID_PKT = -1
+
 # Check if incoming packet is valid and if it's coming from red or black network
 # For example: check existance of IP header, if source IP is known...
 def check_pkt(pkt, red_machine_ip, black_machine_ip):
