a289c1713586fccd4e0b7b4a84380c09800ac08b
Adam Antoshin <adam.antoshin@gmail.com>
2025-03-24 00:44:26 +0200

    Create reconstruct_decrypted_pkt.py

---
+++ projects/encryptor/tashtiot_utils/reconstruct_decrypted_pkt.py
@@ -0,0 +1,6 @@
+from scapy.all import *
+
+# Reconstruct packet with decrypted payload bytes to send out
+# With updated addresses
+def reconstruct_decrypted_pkt(decrypted_payload):
+    return "Replace me with a packet!"
